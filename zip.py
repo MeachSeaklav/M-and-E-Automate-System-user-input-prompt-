a6a6075c02ac8df1af05dff581580f85f4dc8132
MeachSeaklav <143688280+MeachSeaklav@users.noreply.github.com>
2024-09-05 15:16:38 +0700

    Update zip.py

--- zip.py
+++ zip.py
@@ -6,6 +6,6 @@ def create_zip_file(word_filename, pdf_filename, zip_filename):
         with zipfile.ZipFile(zip_filename, 'w') as zipf:
             zipf.write(word_filename)
             zipf.write(pdf_filename)
-        st.success(f"Zip file {zip_filename} created successfully.")
+        # st.success(f"Zip file {zip_filename} created successfully.")
     except Exception as e:
         st.error(f"Failed to create zip file: {e}")
