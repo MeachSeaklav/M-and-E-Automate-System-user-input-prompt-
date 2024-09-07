d968b2161ff39f070858d3d6a9e5c2598a03e059
MeachSeaklav <143688280+MeachSeaklav@users.noreply.github.com>
2024-09-07 17:42:44 +0700

    Update ds.py

--- ds.py
+++ ds.py
@@ -73,7 +73,7 @@ def dashboard():
                 .table-container {
                     margin: 0px; 
                     border-radius: 10px;
-                    height: 385px;
+                    height: 450px;
                     width: 1010px;
                     overflow: auto;
                 }
