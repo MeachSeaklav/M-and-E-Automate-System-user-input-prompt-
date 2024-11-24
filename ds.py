f81f2c65cc5495ebb0e5381791f3451e5c5feae0
MeachSeaklav <143688280+MeachSeaklav@users.noreply.github.com>
2024-11-24 16:36:54 +0700

    Update ds.py

--- ds.py
+++ ds.py
@@ -50,7 +50,7 @@ def dashboard():
         <div style="display: flex; align-items: center;">
             <img src="https://cdn3d.iconscout.com/3d/free/thumb/free-line-chart-growth-3814121-3187502.png" alt="logo" style="width: 70px; margin-right: 15px;">
             <h5 style="font-family: 'Khmer OS Muol Light', Arial, sans-serif; margin-top: 25px;">ប្រព័ន្ធគ្រប់គ្រងព័ត៌មានជលផល</h5>
-            <h3 style="font-family: 'Khmer OS Muol Light', Arial, sans-serif; margin-top: 0; font-size: 18px; font-weight: bold; vertical-align: middle;">Fisheries Information Management System (FIMS)</h3><br><br><br>
+            <h5 style="font-family: 'Khmer OS Muol Light', Arial, sans-serif; margin-top: 0; font-size: 18px; font-weight: bold; vertical-align: middle;">Fisheries Information Management System (FIMS)</h5>
         </div>
     </div>
     <br><br>
